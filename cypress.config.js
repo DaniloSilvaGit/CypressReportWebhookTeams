6f92e3466042ce93152bc2987e772125eb68e828
Danilo Vasconcelos Silva <danilo.silva.vasconcelos7@gmail.com>
2026-07-21 18:19:24 -0300

    Config Mocha Report JSON And HTML

--- cypress.config.js
+++ cypress.config.js
@@ -3,10 +3,31 @@ const { plugin } = require('@cypress/grep/plugin')
 
 module.exports = defineConfig({
   allowCypressEnv: false,
+  projectId: '81hcng',
+  numTestsKeptInMemory: 1,
+  defaultCommandTimeout: 16000,
+  requestTimeout: 16000,
+  responseTimeout: 16000,
+
 
   e2e: {
     setupNodeEvents(on, config) {
       return plugin(config)
     },
+    env: {
+      hideCredentials: true,
+      //requestMode:false,
+    },
+    //reporter: 'mochawesome',
+    reporter: 'cypress-multi-reporters',
+    reporterOptions: {
+      configFile: 'reporter-config.json',
+    },
+    "viewportWidth": 1920,
+    "viewportHeight": 1080,
+    "chromeWebSecurity": false,
+    testIsolation: false,
+    video: true
+
   },
 })
